ce6494d7c12071a21ea34b9e8906cfb9e90141d6
unknown <anton.herzog@AHerzog-PC.sda.local>
2015-10-05 12:07:40 +0200

    Added script stub for adding env-variables.

---
+++ SetEnviromentVariables.ps1
@@ -0,0 +1,9 @@
+﻿#This scripts assumes that all applications are installed in their default directory
+
+$enviromentAdditions = @{
+                            #Add variables here
+                        }
+
+foreach($key in $enviromentAdditions.Keys){
+[Environment]::SetEnvironmentVariable($key,$enviromentAdditions[$key],"Machine")
+}
